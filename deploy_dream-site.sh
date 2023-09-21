6bffc9e4a680305ac742b819211451f13fa6f190
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-09-20 18:13:14 -0700

    Update deploy script

--- deploy_dream-site.sh
+++ deploy_dream-site.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo -u dreamweb git -C ./dream-site pull origin main
 
+# Update config.ini version with git commit hash
+sed -i -e "/version =/ s/= .*/= $(sudo -u dreamweb git -C ./dream-site/ rev-parse HEAD)/" ./dream-site/includes/config/config.ini
+
 sudo chown -R dreamweb:dreamweb ./dream-site/* 2>/dev/null
 sudo chown -R dreamweb:webdev ./dream-site/includes/config 2>/dev/null
 sudo chown -R apache:dreamweb ./dream-site/www/theme/dreamdinners/images/charts/* 2>/dev/null
